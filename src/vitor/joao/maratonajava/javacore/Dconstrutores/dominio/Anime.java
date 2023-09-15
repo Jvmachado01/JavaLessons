19fe619b84be9f28fd1bbcc55ee1f3c9c10a5bb1
Jvmachado01 <joaommachado89@outlook.com>
2023-09-15 14:22:30 -0300

    Java Lessons. Aula sobre construtores de classe

--- src/vitor/joao/maratonajava/javacore/Dconstrutores/dominio/Anime.java
+++ src/vitor/joao/maratonajava/javacore/Dconstrutores/dominio/Anime.java
@@ -1,22 +1,31 @@
-package vitor.joao.maratonajava.javacore.Csobrecargametodos.dominio;
-
-import org.w3c.dom.ls.LSOutput;
+package vitor.joao.maratonajava.javacore.Dconstrutores.dominio;
 
 public class Anime {
     private String nome;
     private String tipo;
     private int episodios;
     private String genero;
-    public void init(String nome, String tipo, int episodios) {
+    private String estudio;
+
+    public Anime(String nome, String tipo, int episodios, String genero) {
+        this();
         this.nome = nome;
         this.tipo = tipo;
         this.episodios = episodios;
-    }
-
-    public void init(String nome, String tipo, int episodios, String genero) {
-        this.init(nome, tipo, episodios);
         this.genero = genero;
     }
+
+    // Overload (sobrecarga) de construtor. Sintaxe this(); só é possível dentro de construtorres
+    // O construtor de overload deve ser chamado na primeira linha do código.
+    public Anime(String nome, String tipo, int episodios, String genero, String estudio) {
+        this(nome, tipo, episodios, genero);
+        this.estudio = estudio;
+    }
+
+    public Anime() {
+        System.out.println("Dentro do construtor sem argumentos");
+    }
+
 
 
     public void imprime() {
@@ -24,6 +33,7 @@ public class Anime {
         System.out.println(this.tipo);
         System.out.println(this.episodios);
         System.out.println(this.genero);
+        System.out.println(this.estudio);
     }
 
     public void setNome(String nome) {
